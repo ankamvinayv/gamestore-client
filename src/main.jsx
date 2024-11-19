ab8a38b8d3d751a2d146c9ed6d0c5717f9e3e2c5
ankamvinayv <ankamvinayv@gmail.com>
2024-11-19 19:44:37 +0530

    glitch recovered

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 
 import { createRoot } from 'react-dom/client';
 import App from './App'; // Import App component 
-import './index.css'; // Tailwind CSS or other global styles
+import './assets/css/style.css'
 
 // Mount the React app to the DOM
 createRoot(document.getElementById('root')).render(
